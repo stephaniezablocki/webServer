452cb9934468c1f28196196bbe0729caa0d97cd9
Stephanie Zablocki <stephaniezablocki@gmail.com>
2021-10-07 20:28:48 -0400

    Made a socket object

---
+++ SimpleSocket.cpp
@@ -0,0 +1,42 @@
+#include "SimpleSocket.hpp"
+
+//Default constructor
+SS::SimpleSocket::SimpleSocket(int domain, int service, int protocol, int port, u_long interface)
+{
+    //Define address structure
+    address.sin_family = domain;
+    address.sin_port = htons(port);
+    address.sin_addr.s_addr = htonl(interface);
+    //Establish socket
+    sock = socket(domain, service, protocol);
+    test_connection(sock);
+    //Establish network connection
+    connection = connect_to_network(sock, address);
+    test_connection(connection);
+}
+
+//Test connection virtual function
+void SS::SimpleSocket::test_connection(int item_to_test)
+{
+    //Confirms that the socket or connection has been properly established
+    if (item_to_test < 0)
+    {
+        perror("Failed to connect...");
+        exit(EXIT_FAILURE);
+    }
+}
+
+struct sockaddr_in SS::SimpleSocket::get_address()
+{
+    return address;
+}
+
+int SS::SimpleSocket::get_sock()
+{
+    return sock;
+}
+
+int SS::SimpleSocket::get_connection()
+{
+    return connection;
+}
